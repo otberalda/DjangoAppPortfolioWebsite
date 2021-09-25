75c5866f7549289d8e356e29e2cb8578dfb04ee8
otberalda <59591834+otberalda@users.noreply.github.com>
2021-09-25 00:00:18 -0600

    added favicon

--- PortfolioWebsite/PortfolioWebsite/settings.py
+++ PortfolioWebsite/PortfolioWebsite/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/aigdev/DjangoAppPortfolioWebsite/PortfolioWebsite/static'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
