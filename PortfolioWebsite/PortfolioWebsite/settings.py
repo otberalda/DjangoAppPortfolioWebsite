c819649857715500a9fbef033d2eec2be9bc91e1
otberalda <aigdev@4b5d82362e5e>
2021-10-06 07:41:56 +0000

    adjusted sizing on objects, made landscape look great on mobile

--- PortfolioWebsite/PortfolioWebsite/settings.py
+++ PortfolioWebsite/PortfolioWebsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import environ
+
 import os
 from pathlib import Path
 
@@ -18,8 +18,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-environ.Env.read_env()
+import os
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -29,15 +28,17 @@ SECRET_KEY = get_random_string(50, chars)
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = env("USER")
-EMAIL_HOST_PASSWORD = env("PASSWORD")
+EMAIL_HOST_USER = os.getenv("EMAILUSER")
+print(EMAIL_HOST_USER)
+EMAIL_HOST_PASSWORD = os.getenv("PASSWORD")
+print(EMAIL_HOST_PASSWORD)
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.adalbertoigarcia.com', 'www.webapp-1195540.pythonanywhere.com']
 
 # Application definition
 
@@ -127,11 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/aigdev/DjangoAppPortfolioWebsite/PortfolioWebsite/static'
 MEDIA_URL = '/images/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
